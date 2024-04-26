c59bd2f906a6372f32df1dd40ab9ae949e9b7513
calibosocarlo1201 <calibosocarlo1201@gmail.com>
2024-04-27 00:24:30 +0800

    Added Jwt token and middlewares

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,27 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 //import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import cookieParser from 'cookie-parser';
 const port = process.env.PORT || 5000;
 
 connectDB(); // connect to monogdb
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Cookie Parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API IS RUNNING...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
